e1667f7a99fc1f6d1d1b40a03c4d68c50d88c548
Denky <sorek230@gmail.com>
2019-05-08 20:20:23 +0200

    Fixed login form

--- src/java/persistence/EJBMusic.java
+++ src/java/persistence/EJBMusic.java
@@ -7,7 +7,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
-import javax.persistence.Query;
 
 /**
  *
